c5748f02c0cc45318a6d6c7c238f93405279a7c0
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-01-13 15:42:44 +0530

    Update projects

--- src/components/Project/info.js
+++ src/components/Project/info.js
@@ -1,44 +1,30 @@
-import project1img from "../../assets/project1.png"
-import project2img from "../../assets/project2.png"
+// import project1img from "../../assets/project1.png"
+// import project2img from "../../assets/project2.png"
 import project3img from "../../assets/project3.png"
 import project4img from "../../assets/project4.png"
 import project5img from "../../assets/project5.png"
 
 const info = {
-    project1: {
-        img: project1img,
+    project3: {
+        img: project3img,
         link: "https://gemini-fe.netlify.app",
         githubLink: "https://github.com/Aymmaann/Gemini",
         heading: "Gemini",
         description: "This repository contains a React application that serves as a clone of the Gemini application, leveraging the Gemini API to provide similar functionalities. The project demonstrates the use of modern React features and RESTful API integration to replicate the core functionalities of Gemini."
     },
-    project2: {
-        img: project2img,
-        link: "https://countries-fe.netlify.app/",
-        githubLink: "https://github.com/Aymmaann/Countries",
-        heading: "Countries",
-        description: "This project is a dynamic web application designed to provide users with information about countries worldwide. Built using modern web technologies including HTML5, CSS3, and JavaScript (ES6+), the application offers an intuitive user interface that supports features such as searching for countries, filtering by region, and displaying detailed info."
-    },
-    project3: {
-        img: project3img,
+    project4: {
+        img: project4img,
         link: "https://edtechfe.netlify.app/",
         githubLink: "https://github.com/Aymmaann/EdTech",
         heading: "EdTech",
         description: "A modern, responsive landing page built with React and Tailwind CSS. Showcasing a course offering with interactive features and a clean design, this project highlights course benefits, testimonials, and pricing. Perfect for demonstrating a sleek and functional course or community webpage."
     },
-    project4: {
-        img: project4img,
+    project5: {
+        img: project5img,
         link: "https://shortly-fe.netlify.app/",
         githubLink: "https://github.com/Aymmaann/Shortly",
         heading: "Shortly",
         description: "Shortly is a streamlined web application built for managing and shortening URLs. It allows users to efficiently shorten links while offering powerful tools for tracking and analyzing their performance. The interface is intuitive, making it simple to organize and manage multiple links, and optimize URL sharing across platforms."
-    },
-    project5: {
-        img: project5img,
-        link: "https://booxx.netlify.app/",
-        githubLink: "https://github.com/Aymmaann/Boox",
-        heading: "Boox",
-        description: "Boox is a user-friendly online bookstore built using React, allowing users to explore a curated collection of books, seamlessly add them to their cart, adjust quantities, and complete their purchase with ease. Designed for a smooth and intuitive shopping experience, Boox brings the joy of discovering and buying books online."
     }
 }
 
