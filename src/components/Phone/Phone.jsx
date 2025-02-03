844cbd2cc5a1f53ff9d3a15111a487801932648d
Ayman Haseeb <aymanhaseeb8121@gmail.com>
2025-02-03 21:34:27 +0530

    Update Styles

--- src/components/Phone/Phone.jsx
+++ src/components/Phone/Phone.jsx
@@ -1,29 +1,42 @@
-import React from "react";
-import "./Phone.css"
+import React, { useEffect } from "react";
+import "./Phone.css";
+import gsap from "gsap";
 import designs from "./designs";
 
 const Phone = () => {
-    return(
-        <>
-            <section className="design-section">
-                <h1 className='text-purple uppercase font-bold text-center mt-2 xl:mt-14 pt-12 l:pt-14 text-[28px] sl:text-[37px] xl:text-[45px] xxl:text-[55px]' id="Designs">Designs</h1>
-                <div className="phone-scroller">
-                    <ul className="phone-scroller-ul">
-                        {Object.values(designs).map((design,index) => (
-                            <li className="one-design" key={index}>
-                                <div className="phone-bezel">
-                                    <div className="dynamic-island"></div>
-                                    <a href={design.link} target="_blank">
-                                        <img src={design.img} alt={`design-${index}`} className="phone-screen xl:w-[175px]"/>
-                                    </a>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+  useEffect(() => {
+    const scrollDuration = 35; 
+    const scroller = document.querySelector('.phone-scroller-ul');
+    scroller.innerHTML += scroller.innerHTML;
+    gsap.to(scroller, {
+      x: `-=${scroller.scrollWidth / 2}`, 
+      duration: scrollDuration,
+      ease: 'linear',
+      repeat: -1, 
+    });
+  }, []);
+
+  return (
+    <>
+      <section className="design-section">
+        <h1 className='text-purple uppercase font-bold text-center mt-2 xl:mt-14 pt-12 l:pt-14 text-[28px] sl:text-[37px] xl:text-[45px] xxl:text-[55px]' id="Designs">Designs</h1>
+        <div className="phone-scroller">
+          <ul className="phone-scroller-ul">
+            {Object.values(designs).map((design, index) => (
+              <li className="one-design" key={index}>
+                <div className="phone-bezel">
+                  <div className="dynamic-island"></div>
+                  <a href={design.link} target="_blank">
+                    <img src={design.img} alt={`design-${index}`} className="phone-screen xl:w-[175px]"/>
+                  </a>
                 </div>
-            </section>
-        </>
-    )
-}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Phone;
